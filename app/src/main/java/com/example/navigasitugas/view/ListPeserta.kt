f2877eb62f448169571d34f7b536db81ba47043d
finanovita <finanovita479@gmail.com>
2025-11-08 11:04:27 +0700

    Menambahkan bottom bar navigasi

--- app/src/main/java/com/example/navigasitugas/view/ListPeserta.kt
+++ app/src/main/java/com/example/navigasitugas/view/ListPeserta.kt
@@ -21,3 +21,23 @@ fun ListPesertaScreen(navController: NavController) {
         topBar = {
             CenterAlignedTopAppBar(title = { Text(stringResource(id = R.string.list_daftarpeserta)) })
         },
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = { navController.navigate(Navigasi.Splash.name) { popUpTo(Navigasi.Splash.name) { inclusive = true } } },
+                    modifier = Modifier.weight(1f).padding(end = 8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary)
+                ) { Text(stringResource(id = R.string.beranda)) }
+
+                Button(
+                    onClick = { navController.navigate(Navigasi.FormPendaftaran.name) },
+                    modifier = Modifier.weight(1f).padding(start = 8.dp)
+                ) { Text(stringResource(id = R.string.formulir)) }
+            }
+        }
+    ) {
