d6e95a2bb4a88784300de636538dfaaaeab0a258
finanovita <finanovita479@gmail.com>
2025-11-08 11:18:08 +0700

    Menambahkan Enum Navigasi yang mendefinisikan daftar rute untuk Splash, ListPeserta, dan FormPendaftaran sebagai navigasi utama aplikasi

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -1,2 +1,25 @@
 package com.example.navigasitugas
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasitugas.view.FormPendaftaranScreen
+import com.example.navigasitugas.view.ListPesertaScreen
+import com.example.navigasitugas.view.SplashScreen
+
+enum class Navigasi {
+    Splash,
+    ListPeserta,
+    FormPendaftaran
+}
+
+@Composable
+fun AppNavigation(
+    navController: NavHostController = rememberNavController()
+) {
