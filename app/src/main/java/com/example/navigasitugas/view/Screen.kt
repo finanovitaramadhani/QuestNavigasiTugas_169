a443cfa3053cf697f6e7c6591231606726ec11f0
finanovita <finanovita479@gmail.com>
2025-11-08 11:11:46 +0700

    Menambahkan struktur utama mengatur layout dasar menggunakan Column dengan alignment dan padding

--- app/src/main/java/com/example/navigasitugas/view/Screen.kt
+++ app/src/main/java/com/example/navigasitugas/view/Screen.kt
@@ -1,2 +1,28 @@
 package com.example.navigasitugas.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.navigasitugas.Navigasi
+import com.example.navigasitugas.R
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+
