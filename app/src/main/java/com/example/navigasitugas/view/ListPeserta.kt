c91ec7e2a80ba7a27280225e98005b10135bff35
finanovita <finanovita479@gmail.com>
2025-11-08 11:03:08 +0700

    Menambahkan Struktur Dasar Scaffold dan TopBar

--- app/src/main/java/com/example/navigasitugas/view/ListPeserta.kt
+++ app/src/main/java/com/example/navigasitugas/view/ListPeserta.kt
@@ -1,2 +1,23 @@
+List Peserta
+
 package com.example.navigasitugas.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.navigasitugas.Navigasi
+import com.example.navigasitugas.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListPesertaScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text(stringResource(id = R.string.list_daftarpeserta)) })
+        },
