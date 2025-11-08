ff009d23f791ac3dd1279c1be71c34f058580dc8
finanovita <finanovita479@gmail.com>
2025-11-08 10:49:27 +0700

    Menambahkan Struktur Dasar FormPendaftaranScreen

--- app/src/main/java/com/example/navigasitugas/view/Formulir.kt
+++ app/src/main/java/com/example/navigasitugas/view/Formulir.kt
@@ -1,2 +1,22 @@
 package com.example.navigasitugas.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.navigation.NavController
+import com.example.navigasitugas.Navigasi
+import com.example.navigasitugas.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormPendaftaranScreen(navController: NavController) {
+
